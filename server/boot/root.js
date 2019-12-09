4f6d3ea5f9aefa86ff1206b5cf58aa8497b0f492
Luc Deschenaux <luc.deschenaux@freesurf.ch>
2019-12-09 11:49:06 +0100

    add/remove comments

--- server/boot/root.js
+++ server/boot/root.js
@@ -3,6 +3,8 @@
 var path=require('path');
 module.exports = function(app) {
   var router = app.loopback.Router();
+  // serve static files from client/dist/ on production mode, unless cookie debug is true.
+  // else serve static files from client/app/
   var index=path.resolve(
     __dirname,
     '..',
@@ -11,7 +13,7 @@ module.exports = function(app) {
     (app.get('production')&&!req.cookies.debug)?'dist':'app',
     'index.html'
   );
-
+  // redirect / to index.html
   router.get('/', function(req,res,next){
     res.sendFile(index, function(err){
       if(err) {
--- server/boot/static.js
+++ server/boot/static.js
@@ -4,6 +4,8 @@ module.exports = function(app) {
   var path=require('path');
   var loopback=require('loopback');
 
+  // serve static files from client/dist/ on production mode, unless cookie debug is true.
+  // else serve static files from client/app/
   app.use(function(req,res,next){
     var staticServe=loopback.static(
       path.resolve(
